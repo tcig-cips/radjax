fixed nans in backprop, neural image recovery works (no pressure velocity)

inference.py
@@ -65,7 +65,7 @@ class DiskSampler(object):
         
         # Einstein coefficient for spontaneous emission from level u to level d
         n_up, n_dn = line_rte.n_up_down(gas_nd, gas_t, self.energy_levels, self.radiative_transitions, transition=disk.transition)
-        alpha_tot = line_rte.alpha_total_co(disk.v_turb, gas_t, disk.m_mol)
+        alpha_tot = line_rte.alpha_total_co(disk.v_turb, gas_t)
 
         model = jnp.nan_to_num(line_rte.compute_spectral_cube(
             freqs, gas_v, alpha_tot, n_up, n_dn, self.a_ud, self.b_ud, self.b_du, self.ray_coords, self.obs_dir, self.nu0, self.pixel_area

line_rte.py
@@ -105,7 +105,7 @@ def compute_spectral_cube(camera_freqs, gas_v, alpha_tot, n_up, n_dn, a_ud, b_ud
     # Second order integration of the source
     # Notes: https://www.ita.uni-heidelberg.de/~dullemond/software/radmc-3d/manual_radmc3d/imagesspectra.html#sec-second-order
     s_nu = j_nu / (a_nu + 1e-30)   # Radmc3d has +1e-99 but this results in nans
-    beta = (dtau - 1 + jnp.exp(-dtau)) / dtau
+    beta = (dtau - 1 + jnp.exp(-dtau)) / (dtau + 1e-30)
     beta = jnp.where(dtau > 1e-6, beta, 0.5*dtau)
     source_2nd = (1 - jnp.exp(-dtau) - beta) * s_nu[...,:-1] + beta * s_nu[...,1:]
     source_2nd = jnp.where(source_2nd < source_1st, source_2nd, source_1st)
@@ -130,7 +130,7 @@ compute_spectral_cube_pmap = jax.pmap(
     in_axes=(0, None, None, None, None, None, None, None, None, None, None, None)
 )
 
-def alpha_total_co(v_turb, gas_t, m_mol):
+def alpha_total_co(v_turb, gas_t):
     """
     Compute the total line broadening due to thermal and turbulence velocities
 
@@ -140,12 +140,9 @@ def alpha_total_co(v_turb, gas_t, m_mol):
         turbulence broadening scaled by the speed of sound.
     gas_t: jnp.array
         Temperature field (K)
-    m_mol: float,
-        Molecular weight
     """
     alpha_therm_sq = 2*kk*gas_t / m_co
-    cs_sq = 2*kk*gas_t / m_mol  # local speed of sound
-    alpha_tot = jnp.sqrt(alpha_therm_sq + v_turb**2 * cs_sq)
+    alpha_tot = jnp.sqrt(alpha_therm_sq * (1 + v_turb**2))
     return alpha_tot
     
 def load_molecular_tables(path):

network.py
@@ -12,23 +12,23 @@ class PixelPredictor(nn.Module):
     posenc_deg: int = 3
     posenc_var: float = 0.0
     net_depth: int = 4
-    net_width: int = 128
-    activation: Callable[..., Any] = nn.sigmoid
+    net_width: int = 64
+    activation: Callable[..., Any] = nn.relu
     out_channel: int = 1
     out_activation: Callable[..., Any] = nn.sigmoid
     do_skip: bool = True
-    sigmoid_offset: float = 10.0
+    min_val: float = 0.0
+    max_val: float = 1.0
+    offset: float = 0.0
     
     def init_params(self, coords, seed=1):
         params = self.init(jax.random.PRNGKey(seed), coords)['params']
-        return params.unfreeze() # TODO(pratul): this unfreeze feels sketchy
+        return params
 
     def init_state(self, params, num_iters=5000, lr_init=1e-4, lr_final=1e-6, checkpoint_dir=''):
         lr = optax.polynomial_schedule(lr_init, lr_final, 1, num_iters)
         tx = optax.adam(learning_rate=lr)
         state = train_state.TrainState.create(apply_fn=self.apply, params=params, tx=tx) 
-        # Replicate state for multiple gpus
-        state = flax.jax_utils.replicate(state)
         return state
     
     @nn.compact
@@ -44,9 +44,9 @@ class PixelPredictor(nn.Module):
             net_output = pixel_mlp(input)
             if self.out_channel == 1:
                 net_output = net_output[..., 0]
+
         
-            offset = self.sigmoid_offset if self.out_activation.__name__ == 'sigmoid' else 0
-            pixels = self.out_activation(net_output - offset)
+            pixels = self.out_activation(net_output-self.offset)*(self.max_val - self.min_val) + self.min_val
             return pixels
         pixels = predict_pixels(coords)
         return pixels
@@ -124,58 +124,11 @@ def posenc(x, deg):
                      list(x.shape[:-1]) + [-1])
     four_feat = safe_sin(jnp.concatenate([xb, xb + 0.5 * jnp.pi], axis=-1))
     return jnp.concatenate([x] + [four_feat], axis=-1)
-
-
-def integrated_posenc(x, max_deg, x_cov, min_deg=0):
-    """
-    Encode `x` with sinusoids scaled by 2^[min_deg:max_deg-1].
-
-    Parameters
-    ----------
-    x: jnp.ndarray, variables to be encoded. Should
-      be in [-pi, pi]. 
-    max_deg: int, the max degree of the encoding.
-    x_cov: jnp.ndarray, covariance matricfes for `x`.
-    min_deg: int, the min degree of the encoding. default=0.
-
-    Returns
-    -------
-    encoded: jnp.ndarray, encoded variables.
-    """
-    if jnp.isscalar(x_cov):
-        x_cov = jnp.full_like(x, x_cov)
-    scales = 2**jnp.arange(min_deg, max_deg)
-    shape = list(x.shape[:-1]) + [-1]
-    y = jnp.reshape(x[..., None, :] * scales[:, None], shape)
-    y_var = jnp.reshape(x_cov[..., None, :] * scales[:, None]**2, shape)
-
-    return expected_sin(
-      jnp.concatenate([y, y + 0.5 * jnp.pi], axis=-1),
-      jnp.concatenate([y_var] * 2, axis=-1))
-
-def expected_sin(x, x_var):
-    # When the variance is wide, shrink sin towards zero.
-    y = jnp.exp(-0.5 * x_var) * jnp.sin(x)
-    return y
-
     
 def shard(xs):
     """Split data into shards for multiple devices along the first dimension."""
     return jax.tree_map(lambda x: x.reshape((jax.local_device_count(), -1) + x.shape[1:]), xs)
 
-"""
-def shard(xs, overlap=0):
-    n_gpus = jax.local_device_count()
-    if xs.shape[0] % n_gpus != overlap:
-        raise AttributeError
-    if overlap == 0:
-        output = jax.tree_map(lambda x: x.reshape((n_gpus, -1) + x.shape[1:]), xs)    
-    elif overlap > 0:
-        output = jax.tree_map(lambda x: x.reshape((n_gpus, x.shape[0] // n_gpus) + x.shape[1:]), xs[:-overlap])
-        output = jnp.concatenate((output, jnp.concatenate((output[1:,0], xs[-1][None]))[:,None]), axis=1)
-
-    return output
-"""   
 
 safe_sin = lambda x: jnp.sin(x % (100 * jnp.pi))
 

parametric_disk.py
@@ -70,11 +70,10 @@ class DiskFlaherty(object):
 
     def velocity(self, z, r, nd, temperature):
         """Modified Keplerian velocity to account for height and gas pressure above the midplane"""
-        v = jnp.sqrt( vsq_keplerian_vertical(z, r, self.M_star) + vsq_pressure_grad(r, nd, temperature, self.m_mol) )
-
+        # v = jnp.sqrt( vsq_keplerian_vertical(z, r, self.M_star) + vsq_pressure_grad(r, nd, temperature, self.m_mol) )
+        v = jnp.sqrt( vsq_keplerian_vertical(z, r, self.M_star) )
         # If there is a large negative pressure gradient this sqrt might produce nans
-        v = jnp.nan_to_num(v)
-        
+        # v = jnp.nan_to_num(v)
         return v
         
     def tree_flatten(self):
@@ -104,65 +103,6 @@ class DiskFlaherty(object):
             self.T_mid1,self.T_atm1, self.q,  self.q_in, self.r_break, self.M_star, self.gamma, self.r_in, self.log_r_c, 
             self.M_gas, self.v_turb, self.co_abundance, self.N_dissoc_lo, self.N_dissoc_hi, self.z_q0, self.transition, 
             self.m_mol, self.freezeout, self.delta, self.r_scale, self.molecule_table, name, memo))
-    
-
-class DiskWilliams(object):
-    """
-    A parameteric disk model based on Williams et al. (2014)
-    
-    Parameters
-    ----------
-    molecule_table: path to the molecular table (e.g. molecule_12c16o.inp)
-    transition: rotational transition index (e.g. 3 is CO(3-2) at ~345 GHz)
-    freezeout: Kelvin. Freezeout temperature of the molecule (e.g. 20K for CO)
-    delta: describe the steepness of the profile (multiplies the power of the sinusoid)
-    r_scale: scaling in AU (default = 150AU)
-
-    Notes
-    -----
-    https://iopscience.iop.org/article/10.1088/0004-637X/788/1/59
-    """
-    def __init__(self, name: str, T_mid1: float, T_atm1: float, q: float, M_star: float, gamma: float, r_in: float, 
-                 r_c: float, M_gas: float, alpha_turb: float, co_abundance: float, N_dissoc: float, 
-                 molecule_table: str, transition: int, m_mol: float = 2.37*m_h, freezeout: float = 20.0, delta: float = 2, r_scale: float = 1.0):
-        self.name = name
-        self.T_mid1 = T_mid1
-        self.T_atm1 = T_atm1
-        self.q = q
-        self.M_star = M_star
-        self.gamma = gamma
-        self.r_in = r_in
-        self.r_c = r_c
-        self.M_gas = M_gas
-        self.alpha_turb = alpha_turb
-        self.co_abundance = co_abundance
-        self.N_dissoc = N_dissoc
-        self.z_q0 = z_q0
-        self.molecule_table = molecule_table
-        self.transition = transition
-        self.m_mol = m_mol
-        self.freezeout = freezeout
-        self.delta = delta
-        self.r_scale = r_scale
-
-    def temperature_profile(self, z, r):
-        T_mid = self.T_mid1 * (r/self.r_scale)**(-self.q)
-        T_atm = self.T_atm1 * (r/self.r_scale)**(-self.q)
-    
-        # The temperature profile is a combination of T_mid, T_atm 
-        # z_q describes the hight at which the disk transitions to the atmopheric value.
-        # H_p is the pressure scale hight derived from the midplane tempertaure
-        H_p = (kk * T_mid * (r * au)**3 / (G*M_star*m_mol))**0.5
-        z_q = 4*H_p/au
-        
-        # The temperature profile is a combination of T_mid, T_atm 
-        temperature = np.where(np.abs(z)<z_q, T_mid + (T_atm - T_mid)*jnp.sin(jnp.pi*z/(2*z_q))**(2*self.delta), T_atm)
-        return temperature
-
-    def co_abundance_profile(self, N_h2, temperature):
-        co_region = jnp.bitwise_and(temperature>self.freezeout, N_h2>self.N_dissoc)
-        abundance = jnp.where(co_region, self.co_abundance, 0.0)
-        return abundance 
 
 def number_density_profile(z, r, temperature, gamma, r_in, r_c, M_gas, M_star, m_mol):
     sigma_0 = (2-gamma) * (M_gas / (2*jnp.pi*(r_c*au)**2)) * jnp.exp(r_in / r_c)**(2-gamma)