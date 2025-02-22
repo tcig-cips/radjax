24f86f06471d0811d5ea609d907c620f39d2f53e
Len <lennemo09@gmail.com>
2025-02-22 15:17:44 -0500

    Syncing WIP code with remote.

--- inference.py
+++ inference.py
@@ -10,6 +10,8 @@ import grid
 import line_rte
 import parametric_disk 
 from consts import *
+from network import shard
+from functools import partial
 
 ###############################################################################
 # Example: Setting up a "Sampler" dictionary and a set of functions that
@@ -112,7 +114,7 @@ def logprior(params, sampler_dict):
     return lp
 
 
-def loglikelihood(params, y, freqs, sampler_dict):
+def loglikelihood(params, y, freqs, stride, sampler_dict):
     """
     Evaluate the log-likelihood by building the disk model and comparing
     to the data.
@@ -182,31 +184,31 @@ def loglikelihood(params, y, freqs, sampler_dict):
     alpha_tot = line_rte.alpha_total_co(disk_params['v_turb'], gas_t)
 
     # 8) Compute the model data cube
-    model_cube = line_rte.compute_spectral_cube(
-        freqs, gas_v, alpha_tot, n_up, n_dn,
+    model_cube = line_rte.compute_spectral_cube_pmap(
+        shard(freqs), gas_v, alpha_tot, n_up, n_dn,
         sampler_dict['a_ud'], sampler_dict['b_ud'], sampler_dict['b_du'],
         sampler_dict['ray_coords'], sampler_dict['obs_dir'],
         sampler_dict['nu0'], sampler_dict['pixel_area']
     )
 
     # 9) Convolve model with the beam
-    model_cube = jnp.nan_to_num(model_cube)
+    model_cube = jnp.nan_to_num(model_cube).reshape(freqs.size, *sampler_dict['ray_coords'].shape[:2])
     model_cube = sensor.fftconvolve_vmap(model_cube, sampler_dict['beam'])
 
     # 10) Evaluate log-likelihood
-    if sampler_dict['stride'] is not None:
-        model_cube = model_cube[:, ::sampler_dict['stride'], ::sampler_dict['stride']]
-        y = y[:, ::sampler_dict['stride'], ::sampler_dict['stride']]
+    if stride is not None:
+        model_cube = model_cube[:, ::stride, ::stride]
+        y = y[:, ::stride, ::stride]
 
     sigma = sampler_dict['sigma']
     logpdf = jsp.stats.norm.logpdf(y, model_cube, sigma)
     return jnp.sum(logpdf)
 
-@jax.jit
-def logprob(params, y, freqs, sampler_dict):
+@partial(jax.jit, static_argnames=['stride']) # TODO: seperate static components from sampler_dict to seperate arg
+def logprob(params, y, freqs, sampler_dict, stride=None):
     """
     jitted version for logprior + loglikelihood.
     """
-    return logprior(params, sampler_dict) + loglikelihood(params, y, freqs, sampler_dict)
+    return logprior(params, sampler_dict) + loglikelihood(params, y, freqs, stride, sampler_dict)
 
 
